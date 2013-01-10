b3a6cf3666934d497ab70da81c24305dedf53735
Maikol <miguel.deelias@gmail.com>
2013-01-10 11:37:00 -0300

    fixing file_name

--- recipes/before_install.rb
+++ recipes/before_install.rb
@@ -7,7 +7,7 @@ end
 edition = "groundworkenterprise"
 groundwork_version = `s3cmd ls s3://Groundwork/ |grep ${edition}- |tail -1 |awk \'{print $4}\'`
 file_name = groundwork_version.split('/')[3]
-file_name = file_name.gsub("\n","")
+default["groundwork"]["file_name"] = file_name.gsub("\n","")
 
 remote_file "/home/ubuntu/#{file_name}" do
         source node["groundwork"]["build_url"] + filename
