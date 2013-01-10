9074a549862f91f45baac3eebfdaa3588e698e73
Maikol <miguel.deelias@gmail.com>
2013-01-10 11:32:06 -0300

    fixing for chef

--- recipes/before_install.rb
+++ recipes/before_install.rb
@@ -6,7 +6,8 @@ end
 
 edition = "groundworkenterprise"
 groundwork_version = `s3cmd ls s3://Groundwork/ |grep ${edition}- |tail -1 |awk \'{print $4}\'`
-file_name = groundwork_version.split('/')[3].gsub("\n","")
+file_name = groundwork_version.split('/')[3]
+file_name = file_name.gsub("\n","")
 
 remote_file "/home/ubuntu/#{file_name}" do
         source node["groundwork"]["build_url"] + filename
