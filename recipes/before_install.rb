20a332ef0aca8eb3c8333f1a7542b0255aa579fc
Maikol <miguel.deelias@gmail.com>
2013-01-10 16:25:01 -0300

    typo

--- recipes/before_install.rb
+++ recipes/before_install.rb
@@ -6,7 +6,7 @@ end
 
 edition = "groundworkenterprise"
 node.set["groundwork"]["s3_location"] = `s3cmd ls s3://Groundwork/ |grep ${edition}- |tail -1 |awk \'{print $4}\'`
-if File.exists?('/home/ubuntu/.s3cmd')
+if File.exists?('/home/ubuntu/.s3cfg')
 	node.set["groundwork"]["file_name"] = node["groundwork"]["s3_location"].split('/')[3].gsub("\n","")
 end
 
