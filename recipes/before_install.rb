4a5263d74f6c8148d983037f4de484463f578e4a
Maikol <miguel.deelias@gmail.com>
2013-01-10 12:43:51 -0200

    bug fix

--- recipes/before_install.rb
+++ recipes/before_install.rb
@@ -7,10 +7,10 @@ end
 edition = "groundworkenterprise"
 groundwork_version = `s3cmd ls s3://Groundwork/ |grep ${edition}- |tail -1 |awk \'{print $4}\'`
 file_name = groundwork_version.split('/')[3]
-default["groundwork"]["file_name"] = file_name.gsub("\n","")
+file_name = file_name.gsub("\n","")
 
 remote_file "/home/ubuntu/#{file_name}" do
-        source node["groundwork"]["build_url"] + filename
+        source node["groundwork"]["build_url"] + file_name
         mode "0644"
         action :create_if_missing
 end
