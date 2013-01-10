5e75db266b6add085e3592d0407047c026e75e48
Maikol <miguel.deelias@gmail.com>
2013-01-10 16:56:45 -0300

    typo

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default["groundwork"]["file_name"] = "groundworkenterprisei.run"
+default["groundwork"]["file_name"] = "groundworkenterprise.run"
 default["groundwork"]["build_url"] = "https://s3.amazonaws.com/Groundwork/"
 default["groundwork"]["s3_location"] = ""
