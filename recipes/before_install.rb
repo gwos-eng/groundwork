b11283441fb23f3e72a00ab226ac5305b9771ea4
Maikol <miguel.deelias@gmail.com>
2013-01-11 09:44:10 -0300

    bug fix

--- recipes/before_install.rb
+++ recipes/before_install.rb
@@ -4,14 +4,13 @@ template "/etc/sysctl.conf" do
         mode "0644"
 end
 
-edition = "groundworkenterprise"
-node.set["groundwork"]["s3_location"] = `s3cmd ls s3://Groundwork/ |grep ${edition}- |tail -1 |awk \'{print $4}\'`
-
 remote_file "/home/ubuntu/#{node["groundwork"]["file_name"]}" do
-	if File.exists?('/home/ubuntu/groundwork.run')
-		node.set["groundwork"]["build_url"] += node["groundwork"]["s3_location"].split('/')[3].gsub("\n","")
-	end
-        source node["groundwork"]["build_url"] 
-        mode "0644"
-        action :create_if_missing
+    if File.exists?('/home/ubuntu/.s3cfg')
+        edition = "groundworkenterprise"
+        s3_location = `s3cmd ls s3://Groundwork/ |grep ${edition}- |tail -1 |awk \'{print $4}\'`
+        build_url = "https://s3.amazonaws.com/Groundwork/" + s3_location.split('/')[3].gsub("\n","")
+    end
+    source build_url
+    mode "0644"
+    action :create_if_missing
 end
