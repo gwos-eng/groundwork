55df1c9385215a29de62be6e04040a6d8f927c50
Maikol <miguel.deelias@gmail.com>
2013-01-11 11:27:29 -0300

    testing

--- recipes/before_install.rb
+++ recipes/before_install.rb
@@ -9,7 +9,7 @@ ruby_block "create variables" do
         edition = "groundworkenterprise"
         s3_location = `s3cmd ls s3://Groundwork/ |grep ${edition}- |tail -1 |awk \'{print $4}\'`
         node.set["groundwork"]["file_name"] = s3_location.split('/')[3].gsub("\n","")
-        node.set["groundwork"]["build_url"] = "https://s3.amazonaws.com/Groundwork/" + node["groundwork"]["file_name"]
+        node.set["groundwork"]["build_url"] = "https://s3.amazonaws.com/Groundwork/#{node["groundwork"]["file_name"]}"
     end
     action :create
     notifies :create_if_missing, "remote_file[Groundwork File]"
