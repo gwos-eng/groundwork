3f78209fa2539bdc61c0724f6d2276f4be7ebe6c
Maikol <miguel.deelias@gmail.com>
2013-01-11 10:36:52 -0300

    bug fix

--- recipes/before_install.rb
+++ recipes/before_install.rb
@@ -9,7 +9,7 @@ ruby_block "create variables" do
         edition = "groundworkenterprise"
         s3_location = `s3cmd ls s3://Groundwork/ |grep ${edition}- |tail -1 |awk \'{print $4}\'`
         node.set["groundwork"]["file_name"] = s3_location.split('/')[3].gsub("\n","")
-        node.set["gourndowkr"]["build_url"] = "https://s3.amazonaws.com/Groundwork/" + file_name
+        node.set["gourndowkr"]["build_url"] = "https://s3.amazonaws.com/Groundwork/" + node["groundwork"]["file_name"]
     end
     action :create
 end
