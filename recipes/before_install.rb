465769a4b978f69ab455e649f8e6a645b2bbaaae
Maikol <miguel.deelias@gmail.com>
2013-01-11 10:35:16 -0300

    adding attributes

--- recipes/before_install.rb
+++ recipes/before_install.rb
@@ -8,14 +8,14 @@ ruby_block "create variables" do
     block do
         edition = "groundworkenterprise"
         s3_location = `s3cmd ls s3://Groundwork/ |grep ${edition}- |tail -1 |awk \'{print $4}\'`
-        file_name = s3_location.split('/')[3].gsub("\n","")
-        build_url = "https://s3.amazonaws.com/Groundwork/" + file_name
+        node.set["groundwork"]["file_name"] = s3_location.split('/')[3].gsub("\n","")
+        node.set["gourndowkr"]["build_url"] = "https://s3.amazonaws.com/Groundwork/" + file_name
     end
     action :create
 end
 
-remote_file "/home/ubuntu/#{file_name}" do
-    source build_url
+remote_file "/home/ubuntu/#{node["groundwork"]["file_name"]}" do
+    source node["groundwork"]["build_url"]
     mode "0644"
     action :create_if_missing
 end
